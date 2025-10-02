4c56502bc208a9afaa3f4f6879879e38b00d1e24
Natan Zagdański <145280000+NatanZSE@users.noreply.github.com>
2025-10-02 11:49:29 +0200

    Add files via upload

--- tabelaaa/aaa.php
+++ tabelaaa/aaa.php
@@ -1,1 +1,12 @@
-
+<head>
+    <link rel="stylesheet" href="aaa.css">
+</head>
+<table>
+<?php
+$con = mysqli_connect("127.0.0.1", "root", "", "napoje");
+$q1 = "SELECT * FROM `picie`";
+if($wynik1 = $con->query($q1))
+while($row1 = $wynik1->fetch_array())
+echo "<tr><td>".$row1["id"]."</td><td>".$row1["nazwa"]."</td><td>".$row1["pojemnosc"]."</td></tr>"
+?>
+</table>
