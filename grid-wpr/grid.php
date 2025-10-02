032f7f2316c3e4e4e93c8a54fe1c0493747ca718
Natan Zagdański <145280000+NatanZSE@users.noreply.github.com>
2025-10-02 11:53:39 +0200

    Add files via upload

--- grid-wpr/grid.php
+++ grid-wpr/grid.php
@@ -1,1 +1,19 @@
-
+<html>
+<head>
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+</head>
+<body>
+    <div id="main">
+<?php
+$con = mysqli_connect("localhost", "root", "", "redshirt");
+$query = "SELECT * FROM uczen";
+$result = mysqli_query($con, $query);
+while($row = mysqli_fetch_array($result)){
+    echo("<div>".$row["id"]." ".$row["Nazwisko"]." ".$row["Imie"]." ".$row["Srednia_ocen"]." ".$row["id_klasy"]."</div>");;
+}
+mysqli_close($con);
+?>
+    </div>
+</body>
+</html>
