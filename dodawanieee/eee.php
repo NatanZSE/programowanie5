656da98bf2cf8a578367a0b56a464714d37cb33c
Natan Zagdański <145280000+NatanZSE@users.noreply.github.com>
2025-10-02 11:52:22 +0200

    Add files via upload

--- dodawanieee/eee.php
+++ dodawanieee/eee.php
@@ -1,1 +1,8 @@
-
+<?php
+$get_id = $_GET['id'];
+$get_kl = $_GET['klasa'];
+$con = mysqli_connect("localhost", "root", "", "redshirt");
+$add = "INSERT INTO `klasa`(`id`, `nazwa`) VALUES ('$get_id', '$get_kl')";
+$result = $con->query($add);
+echo "Do bazy klasa doano klase $get_kl. <br>";
+?>
